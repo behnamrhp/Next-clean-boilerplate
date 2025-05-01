3a0c13111a6062f186e8457e55085bf4c1183558
Behnamrhp74 <behnam.rahimpoor@beatstream.com>
2025-05-01 19:41:19 +0300

    refactor: Remove reduntand backend endpoint

--- src/bootstrap/endpoint/endpoints/backend-endpoints.ts
+++ src/bootstrap/endpoint/endpoints/backend-endpoints.ts
@@ -3,37 +3,11 @@ import Endpoint from "../endpoint";
 
 export default class BackendEndpoint extends Endpoint {
   /* ------------------------------ Dependencies ------------------------------ */
-  private credentialsEndpoint: string;
-
   private usersEndpoint: string;
 
-  private teamsEndpoint: string;
-
-  private membersEndpoint: string;
-
-  private generatePassUsernameEndpoint: string;
-
   /* --------------------------------- Getters -------------------------------- */
-  get credentials() {
-    return this.buildEndpoint(this.credentialsEndpoint);
-  }
-
   get users() {
     return this.buildEndpoint(this.usersEndpoint);
-  }
-
-  get teams() {
-    return this.buildEndpoint(this.teamsEndpoint);
-  }
-
-  get members() {
-    return this.buildEndpoint(this.membersEndpoint);
-  }
-
-  get generatePassUsername() {
-    return this.buildEndpoint(
-      `${this.credentialsEndpoint}/${this.generatePassUsernameEndpoint}`,
-    );
   }
 
   /* ------------------------------- Constructor ------------------------------ */
@@ -42,11 +16,7 @@ export default class BackendEndpoint extends Endpoint {
       apiVersion: "api/v1",
       baseURL: serverConfigs.env.backendApi.url,
     });
-    this.credentialsEndpoint = "credentials";
     this.usersEndpoint = "users";
-    this.generatePassUsernameEndpoint = "username";
-    this.teamsEndpoint = "teams";
-    this.membersEndpoint = "members";
   }
 
   /* -------------------------------------------------------------------------- */
